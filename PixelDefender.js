f358afab2ff94f598f0c51c0cf08681d30b32221
Extered <EXTERED@Gmail.com>
2017-10-12 17:48:52 +0300

    Update PixelDefender.js

--- PixelDefender.js
+++ PixelDefender.js
@@ -1,49 +1,49 @@
 // ==UserScript==
 // @name         Pixel Bot 2ch
 // @namespace    http://tampermonkey.net/
-// @version      1.1.45
+// @version      1.4.88
 // @description  try to take over the world!
 // @author       Flyink13, DarkKeks, xi
 // @match        https://pixel.vkforms.ru/*
-// @downloadURL  https://raw.githubusercontent.com/Extered/pixwars/master/PixelDefender.js
-// @updateURL    https://raw.githubusercontent.com/Extered/pixwars/master/PixelDefender.js
+// @downloadURL  https://raw.githubusercontent.com/Extered/pixwars/gh-pages/PixelDefender.js
+// @updateURL    https://raw.githubusercontent.com/Extered/pixwars/gh-pages/PixelDefender.js
 // @grant        none
 // ==/UserScript==
 
-function MyAwesomePixelBot() {
-    window.MyAwesomePixelBot = MyAwesomePixelBot;
-    MyAwesomePixelBot.url = {
-        script: 'https://raw.githubusercontent.com/Extered/pixwars/master/PixelDefender.js'
+function XIDefBot() {
+    window.XIDefBot = XIDefBot;
+    XIDefBot.url = {
+        script: 'https://extered.github.io/pixwars/PixelDefender.js'
     };
-    MyAwesomePixelBot.refreshTime = 300;
-    MyAwesomePixelBot.pts = 30;
-    MyAwesomePixelBot.tc = "rgb(0, 0, 0)";
-    MyAwesomePixelBot.doCoordLog = true;
-    MyAwesomePixelBot.currentUrl = '';
-    MyAwesomePixelBot.urlGen = {
+    XIDefBot.refreshTime = 300;
+    XIDefBot.pts = 30;
+    XIDefBot.tc = "rgb(0, 0, 0)";
+    XIDefBot.doCoordLog = true;
+    XIDefBot.currentUrl = '';
+    XIDefBot.urlGen = {
         script: function() {
-            return MyAwesomePixelBot.url.script + '?v=' + Math.random()
+            return XIDefBot.url.script + '?v=' + Math.random()
         },
         image: function() {
             return new Promise(function(resolve, reject) {
-                fetch('https://raw.githubusercontent.com/Extered/pixwars/master/defence.json').then(function(data) {
+                fetch('https://raw.githubusercontent.com/Extered/pixwars/gh-pages/defence.json').then(function(data) {
                         data.json().then(function(answer){
                             resolve(answer.currentTarget);
                         }).catch(function(e) {
-                            MyAwesomePixelBot.state.textContent = "E120: " + e;
+                            XIDefBot.state.textContent = "E120: " + e;
                             reject();
                         });
                 }).catch(function(e){
-                    MyAwesomePixelBot.state.textContent = "E121: " + e;                    
-                    resolve(MyAwesomePixelBot.currentUrl);
+                    XIDefBot.state.textContent = "E121: " + e;
+                    resolve(XIDefBot.currentUrl);
                 });
             });
         }
     };
-    MyAwesomePixelBot.state = document.createElement("div");
-    MyAwesomePixelBot.state.onclick = MyAwesomePixelBot.reload;
-    MyAwesomePixelBot.state.textContent = "Загрузка приложения...";
-    Object.assign(MyAwesomePixelBot.state.style, {
+    XIDefBot.state = document.createElement("div");
+    XIDefBot.state.onclick = XIDefBot.reload;
+    XIDefBot.state.textContent = "Загрузка приложения...";
+    Object.assign(XIDefBot.state.style, {
         background: "rgba(0,0,0,0.5)",
         bottom: "0px",
         right: "0px",
@@ -55,10 +55,10 @@ function MyAwesomePixelBot() {
         position: "fixed",
         zIndex: 10000
     });
-    document.body.appendChild(MyAwesomePixelBot.state);
-    MyAwesomePixelBot.loger = document.createElement("div");
-    MyAwesomePixelBot.loger.onclick = MyAwesomePixelBot.reload;
-    Object.assign(MyAwesomePixelBot.loger.style, {
+    document.body.appendChild(XIDefBot.state);
+    XIDefBot.loger = document.createElement("div");
+    XIDefBot.loger.onclick = XIDefBot.reload;
+    Object.assign(XIDefBot.loger.style, {
         background: "rgba(0,0,0,0)",
         top: "0px",
         left: "0px",
@@ -66,55 +66,59 @@ function MyAwesomePixelBot() {
         height: "100%",
         color: "#fff",
         position: "fixed",
-       
+
         fontSize: "11px",
         padding: "12px",
         zIndex: 10001
     });
-    document.body.appendChild(MyAwesomePixelBot.loger);
-    MyAwesomePixelBot.log = function(x) {
-        MyAwesomePixelBot.loger.innerHTML += x + "<br>";
-        MyAwesomePixelBot.loger.scrollTo(0, 10000)
+    document.body.appendChild(XIDefBot.loger);
+    XIDefBot.log = function(x) {
+        XIDefBot.loger.innerHTML += x + "<br>";
+        XIDefBot.loger.scrollTo(0, 10000)
     }
     ;
-    MyAwesomePixelBot.setState = function(s) {
-        MyAwesomePixelBot.state.innerHTML = "MyAwesomePixelBot " + s;
-        MyAwesomePixelBot.log(s)
+    XIDefBot.setState = function(s) {
+        XIDefBot.state.innerHTML = "XIDefBot " + s;
+        XIDefBot.log(s)
     }
     ;
-    MyAwesomePixelBot.reloadImage = function() {
-        MyAwesomePixelBot.urlGen.image().then(function(url) {
-            if (!MyAwesomePixelBot.img) {
-                MyAwesomePixelBot.img = new Image();
-                MyAwesomePixelBot.img.crossOrigin = "Anonymous";
-                MyAwesomePixelBot.img.onload = function() {
-                    MyAwesomePixelBot.setState("перезагрузил зону защиты.");
-                    if (MyAwesomePixelBot.inited) MyAwesomePixelBot.getFullData();
+    XIDefBot.reloadImage = function() {
+        XIDefBot.urlGen.image().then(function(url) {
+            if (!XIDefBot.img) {
+                XIDefBot.img = new Image();
+                XIDefBot.img.crossOrigin = "Anonymous";
+                XIDefBot.img.onload = function() {
+                    XIDefBot.setState("перезагрузил зону защиты.");
+                    if (XIDefBot.inited) XIDefBot.getFullData();
                 };
             }
-            if (url != MyAwesomePixelBot.currentUrl) {
-                MyAwesomePixelBot.img.src = url;
-                MyAwesomePixelBot.currentUrl = url;
+            if (url != XIDefBot.currentUrl) {
+                XIDefBot.img.src = url;
+                XIDefBot.currentUrl = url;
             }
         });
     };
-    MyAwesomePixelBot.canvasEvent = function(type, q) {
-        if (!MyAwesomePixelBot.canvas)
+    XIDefBot.canvasEvent = function(type, q) {
+        if (!XIDefBot.canvas)
             return;
         if (type == "mousewheel") {
-            MyAwesomePixelBot.canvas.dispatchEvent(new WheelEvent("mousewheel",q))
+            XIDefBot.canvas.dispatchEvent(new WheelEvent("mousewheel",q));
         } else {
-            MyAwesomePixelBot.canvas.dispatchEvent(new MouseEvent(type,q))
+            XIDefBot.canvas.dispatchEvent(new MouseEvent(type,q));
         }
     }
     ;
-    MyAwesomePixelBot.canvasClick = function(x, y, color) {
-        MyAwesomePixelBot.resetZoom();
+
+    XIDefBot.parseRgb = function(rgb) {
+        return rgb.replace('rgb(', '').replace(')', '').split(', ').map(function(e){return +e;});
+    };
+    XIDefBot.canvasClick = function(x, y, color) {
+        XIDefBot.resetZoom();
         if (x > 1590) {
-            MyAwesomePixelBot.canvasMoveTo(1590, 0);
+            XIDefBot.canvasMoveTo(1590, 0);
             x = x - 1590
         } else {
-            MyAwesomePixelBot.canvasMoveTo(0, 0)
+            XIDefBot.canvasMoveTo(0, 0)
         }
         var q = {
             bubbles: true,
@@ -125,48 +129,62 @@ function MyAwesomePixelBot() {
             layerX: x,
             layerY: y + 1
         };
-        var pxColor = MyAwesomePixelBot.getColor(MyAwesomePixelBot.ctx.getImageData(x, y + 1, 1, 1).data, 0);
-        var colorEl = document.querySelector('[style="background-color: ' + color + ';"]');
+        var pxColor = XIDefBot.getColor(XIDefBot.ctx.getImageData(x, y + 1, 1, 1).data, 0);
+        var colors = document.getElementsByClassName('color');
+        var colorEl = null;
+        var mindelta = 999999;
+        if (colors.length == 0) {
+            return;
+        }
+        for (var i = 0; i < colors.length; i++) {
+            var colorElRgb = XIDefBot.parseRgb(colors[i].style.backgroundColor);
+            var neededRgb = XIDefBot.parseRgb(color);
+            var delta = Math.abs(colorElRgb[0] - neededRgb[0]) + Math.abs(colorElRgb[1] - neededRgb[1]) + Math.abs(colorElRgb[2] - neededRgb[2]);
+            if (delta < mindelta) {
+                mindelta = delta;
+                colorEl = colors[i];
+            };
+        }
         if (!colorEl) {
             console.log("color error %c " + color, 'background:' + color + ';');
-            MyAwesomePixelBot.setState("Ошибка подбора цвета " + color);
+            XIDefBot.setState("Ошибка подбора цвета " + color);
             return
         } else if (pxColor == color) {
-            if (MyAwesomePixelBot.doCoordLog) {
+            if (XIDefBot.doCoordLog) {
                 console.log("== " + x + "x" + y + "%c " + pxColor, 'background:' + pxColor + ';');
-                MyAwesomePixelBot.setState("Пропускаю " + (x + 1) + "x" + (y + 1) + " совпал цвет")
+                XIDefBot.setState("Пропускаю " + (x + 1) + "x" + (y + 1) + " совпал цвет")
             } else {
                 console.log("==");
-                MyAwesomePixelBot.setState("Пропускаю, совпал цвет")
+                XIDefBot.setState("Пропускаю, совпал цвет")
             }
             return
         } else {
-            if (MyAwesomePixelBot.doCoordLog) {
+            if (XIDefBot.doCoordLog) {
                 console.log(x + "x" + y + "%c " + pxColor + " -> %c " + color, 'background:' + pxColor + ';', 'background:' + color + ';');
-                MyAwesomePixelBot.setState("Поставил точку " + (x + 1) + "x" + (y + 1))
+                XIDefBot.setState("Поставил точку " + (x + 1) + "x" + (y + 1))
             } else {
                 console.log(" -> ");
-                MyAwesomePixelBot.setState("Поставил точку")
+                XIDefBot.setState("Поставил точку")
             }
         }
         colorEl.click();
-        MyAwesomePixelBot.canvasEvent("mousedown", q);
-        MyAwesomePixelBot.canvasEvent("click", q);
+        XIDefBot.canvasEvent("mousedown", q);
+        XIDefBot.canvasEvent("click", q);
         q.button = 0;
-        MyAwesomePixelBot.canvasEvent("mouseup", q);
-        document.querySelector(".App__confirm button").click()
+        XIDefBot.canvasEvent("mouseup", q);
+        document.getElementsByTagName('button')[0].click()
     }
     ;
-    MyAwesomePixelBot.draw = function() {
-        var px = MyAwesomePixelBot.pixs.shift();
+    XIDefBot.draw = function() {
+        var px = XIDefBot.pixs.shift();
         if (!px) {
-            MyAwesomePixelBot.setState("Точек нет")
+            XIDefBot.setState("Точек нет")
         } else {
-            MyAwesomePixelBot.canvasClick(px[0], px[1], px[2])
+            XIDefBot.canvasClick(px[0], px[1], px[2])
         }
     }
     ;
-    MyAwesomePixelBot.canvasMove = function(x, y) {
+    XIDefBot.canvasMove = function(x, y) {
         var q = {
             bubbles: true,
             cancelable: true,
@@ -174,52 +192,52 @@ function MyAwesomePixelBot() {
             clientX: 0,
             clientY: 0
         };
-        MyAwesomePixelBot.canvasEvent("mousedown", q);
+        XIDefBot.canvasEvent("mousedown", q);
         q.clientY = y;
         q.clientX = x;
-        MyAwesomePixelBot.canvasEvent("mousemove", q);
-        MyAwesomePixelBot.canvasEvent("mouseup", q)
+        XIDefBot.canvasEvent("mousemove", q);
+        XIDefBot.canvasEvent("mouseup", q)
     }
     ;
-    MyAwesomePixelBot.canvasMoveTo = function(x, y) {
-        MyAwesomePixelBot.canvasMove(10000, 10000);
-        MyAwesomePixelBot.canvasMove(-40 - x, -149 - y)
+    XIDefBot.canvasMoveTo = function(x, y) {
+        XIDefBot.canvasMove(10000, 10000);
+        XIDefBot.canvasMove(-40 - x, -149 - y)
     }
     ;
-    MyAwesomePixelBot.getImageData = function() {
-        var data = MyAwesomePixelBot.ctx.getImageData(0, 1, 1590, 400).data;
+    XIDefBot.getImageData = function() {
+        var data = XIDefBot.ctx.getImageData(0, 1, 1590, 400).data;
         return data
     }
     ;
-    MyAwesomePixelBot.getColor = function(data, i) {
+    XIDefBot.getColor = function(data, i) {
         return "rgb(" + data[i] + ", " + data[i + 1] + ", " + data[i + 2] + ")"
     }
     ;
-    MyAwesomePixelBot.getFullData = function() {
-        MyAwesomePixelBot.pixs = [];
-        MyAwesomePixelBot.pixs = MyAwesomePixelBot.randomShuffle(MyAwesomePixelBot.getData(0));
-        MyAwesomePixelBot.setState("осталось точек:" + MyAwesomePixelBot.pixs.length);
-        return MyAwesomePixelBot.pixs.length
+    XIDefBot.getFullData = function() {
+        XIDefBot.pixs = [];
+        XIDefBot.pixs = XIDefBot.randomShuffle(XIDefBot.getData(0));
+        XIDefBot.setState("осталось точек:" + XIDefBot.pixs.length);
+        return XIDefBot.pixs.length
     }
     ;
-    MyAwesomePixelBot.getData = function(offsetX) {
-        MyAwesomePixelBot.resetZoom();
-        MyAwesomePixelBot.canvasMoveTo(offsetX, 0);
-        var id1 = MyAwesomePixelBot.getImageData();
-        MyAwesomePixelBot.ctx.drawImage(MyAwesomePixelBot.img, -offsetX, 1);
-        var id2 = MyAwesomePixelBot.getImageData();
+    XIDefBot.getData = function(offsetX) {
+        XIDefBot.resetZoom();
+        XIDefBot.canvasMoveTo(offsetX, 0);
+        var id1 = XIDefBot.getImageData();
+        XIDefBot.ctx.drawImage(XIDefBot.img, -offsetX, 1);
+        var id2 = XIDefBot.getImageData();
         var data = [];
         for (var i = 0; i < id1.length; i += 4) {
             var x = offsetX + (i / 4) % 1590
               , y = ~~((i / 4) / 1590);
-            if (MyAwesomePixelBot.getColor(id1, i) !== MyAwesomePixelBot.getColor(id2, i) && MyAwesomePixelBot.getColor(id2, i) !== MyAwesomePixelBot.tc) {
-                data.push([x, y, MyAwesomePixelBot.getColor(id2, i), MyAwesomePixelBot.getColor(id1, i)])
+            if (XIDefBot.getColor(id1, i) !== XIDefBot.getColor(id2, i) && XIDefBot.getColor(id2, i) !== XIDefBot.tc) {
+                data.push([x, y, XIDefBot.getColor(id2, i), XIDefBot.getColor(id1, i)])
             }
         }
         return data
     };
 
-    MyAwesomePixelBot.randomShuffle = function(data) {
+    XIDefBot.randomShuffle = function(data) {
         var currentIndex = data.length, temporaryValue, randomIndex;
         while (0 !== currentIndex) {
             randomIndex = Math.floor(Math.random() * currentIndex);
@@ -231,71 +249,72 @@ function MyAwesomePixelBot() {
         return data
     };
 
-    MyAwesomePixelBot.resetZoom = function() {
-        MyAwesomePixelBot.canvasEvent("mousewheel", {
+    XIDefBot.resetZoom = function() {
+        XIDefBot.canvasEvent("mousewheel", {
             deltaY: 100000,
             deltaX: 0,
             clientX: 100,
             clientY: 100,
-        })
+        });
     };
 
-    MyAwesomePixelBot.init = function() {
-        MyAwesomePixelBot.inited = 1;
-        MyAwesomePixelBot.getFullData();
-        MyAwesomePixelBot.setState("Запущен.")
+    XIDefBot.init = function() {
+        XIDefBot.inited = 1;
+        XIDefBot.getFullData();
+        XIDefBot.setState("Запущен.")
     };
 
-    MyAwesomePixelBot.wait = setInterval(function() {
+    XIDefBot.wait = setInterval(function() {
         if (window.localStorage.getItem('DROP_FIRST_TIME') != '1') {
             document.querySelector(".App__advance > .Button.primary").click();
         } else if (window.localStorage.getItem('DROP_HEADER') != '1') {
             document.querySelector(".Header__close").click();
-        } else if (!MyAwesomePixelBot.inited && MyAwesomePixelBot.canvas) {
-            MyAwesomePixelBot.ctx = MyAwesomePixelBot.canvas.getContext("2d");
-            MyAwesomePixelBot.init()
-        } else if (MyAwesomePixelBot.canvas && document.querySelector(".Ttl__wait")) {
-            MyAwesomePixelBot.timer = 1
-        } else if (!MyAwesomePixelBot.canvas) {
+        } else if (!XIDefBot.inited && XIDefBot.canvas) {
+            XIDefBot.ctx = XIDefBot.canvas.getContext("2d");
+            XIDefBot.init()
+        } else if (XIDefBot.canvas && document.querySelector(".Ttl__wait")) {
+            XIDefBot.timer = 1
+        } else if (!XIDefBot.canvas) {
             var all = document.querySelectorAll("canvas");
             for(var i = 0; i < all.length; ++i) {
                 if(all[i].style.display != 'none') {
-                    MyAwesomePixelBot.canvas = all[i];
+                    XIDefBot.canvas = all[i];
                 }
             }
-        } else if (!MyAwesomePixelBot.pts) {
-            MyAwesomePixelBot.reload();
-            MyAwesomePixelBot.pts = 30
-        } else if (MyAwesomePixelBot.inited && MyAwesomePixelBot.canvas) {
-            MyAwesomePixelBot.pts--;
-            MyAwesomePixelBot.draw()
+        } else if (!XIDefBot.pts) {
+            XIDefBot.reload();
+            XIDefBot.pts = 30
+        } else if (XIDefBot.inited && XIDefBot.canvas) {
+            XIDefBot.pts--;
+            XIDefBot.draw()
         }
     }, 1000);
 
-    MyAwesomePixelBot.refresh = setTimeout(function() {
+    XIDefBot.refresh = setTimeout(function() {
         location.reload()
-    }, MyAwesomePixelBot.refreshTime * 1e3);
+    }, XIDefBot.refreshTime * 1e3);
 
-    MyAwesomePixelBot.reload = function() {
-        MyAwesomePixelBot.state.outerHTML = "";
-        MyAwesomePixelBot.loger.outerHTML = "";
-        clearInterval(MyAwesomePixelBot.wait);
+    XIDefBot.reload = function() {
+        XIDefBot.state.outerHTML = "";
+        XIDefBot.loger.outerHTML = "";
+        clearInterval(XIDefBot.wait);
         var script = document.createElement('script');
-        script.src = MyAwesomePixelBot.urlGen.script();
+        script.type = 'application/javascript';
+        script.src = XIDefBot.urlGen.script();
         document.body.appendChild(script)
     };
 
-    MyAwesomePixelBot.reloadImage();
-    console.log("MyAwesomePixelBot loaded")
+    XIDefBot.reloadImage();
+    console.log("XIDefBot loaded")
 }
 
 if (window.loaded) {
-    MyAwesomePixelBot();
+    XIDefBot();
 } else {
     var inject = function() {
         window.loaded = 1;
         var script = document.createElement('script');
-        script.appendChild(document.createTextNode('(' + MyAwesomePixelBot + ')();'));
+        script.appendChild(document.createTextNode('(' + XIDefBot + ')();'));
         (document.body || document.head || document.documentElement).appendChild(script);
     };
 
